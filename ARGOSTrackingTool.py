e18c121328f98543f54d0043271e533cfddd737f
Xiaoge Zhang 张宵鸽 <Alice.xiaoge.zhang@gmail.com>
2022-10-01 14:31:40 -0400

    Update ARGOSTrackingTool.py
    task 7 user input

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,6 +9,8 @@
 # Date:   Fall 2022
 #--------------------------------------------------------------
 
+# Ask the user for a date, specifying the format
+user_date = input("Enter a date (M/D/YYYY):")
 
 #Create a variable pointing to the data file
 file_name = './data/raw/Sara.txt'
@@ -42,4 +44,17 @@ for line in line_list:
         location_dict[record_id] = (obs_lat, obs_lon)
     
     # print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    
+#Create an empty key list
+matching_keys = []
 
+# Loop through all key, value pairs in the date_dictionary
+for the_key, the_value in date_dict.items():
+    #See if the date (the value) matches the user date
+    if the_value == user_date:
+        matching_keys.append(the_key)
+
+#Reveal locations for each key in matching_keys
+for matching_key in matching_keys:
+    obs_lat, obs_lon = location_dict[matching_key]
+    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {user_date}")
